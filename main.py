60076305e2eed66be955afd933e9020a8ba17a9d
Giovanna Sousa <giovannasousa105@gmail.com>
2026-01-15 23:32:04 -0300

    backend estável

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class APRCreate(BaseModel):
+    titulo: str
+    risco: str
+    descricao: str | None = None
 
 @app.get("/")
 def root():
     return {"status": "ok"}
 
 @app.post("/aprs")
-def criar_apr():
-    return {"msg": "rota aprs funcionando"}
+def criar_apr(apr: APRCreate):
+    return {
+        "titulo": apr.titulo,
+        "risco": apr.risco,
+        "descricao": apr.descricao
+    }
