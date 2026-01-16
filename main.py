dd67270f5863531eb5c0471010436f48baa9790e
Giovanna Sousa <giovannasousa105@gmail.com>
2026-01-16 13:52:50 -0300

    add listar aprs endpoint

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 from database import SessionLocal
 import models
 import schemas
 from importar_excel import importar_apr_excel
 
-
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+
+@app.get("/aprs")
+def listar_aprs(db: Session = Depends(get_db)):
+    return db.query(models.APR).all()
